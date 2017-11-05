77bc6907ec7a86feffcdd96d0b9df863c8d75ed7
yuduki <rtccb.yuduki24@gmail.com>
2017-11-05 21:28:41 +0900

    [add]ミサイル発射機能の追加.

--- src/invader.py
+++ src/invader.py
@@ -14,8 +14,11 @@ def main():
     # スプライトグループを作成して登録
     all = pygame.sprite.RenderUpdates()
     Player.containers = all
+    Shot.containers = all
     # スプライトの画像を登録
     Player.image, Player.rect = loader.load_image("player.png")
+    Shot.image, Shot.rect = loader.load_image("shot.png")
+    Player.shot_sound = loader.load_sound("shot.wav")
     # 自機を作成
     Player()
     
@@ -37,10 +40,12 @@ def main():
 class Player(pygame.sprite.Sprite):
     """自機"""
     speed = 5  # 移動速度
+    reload_time = 15  # リロード時間
     def __init__(self):
         # imageとcontainersはmain()でセットされる
         pygame.sprite.Sprite.__init__(self, self.containers)
         self.rect.bottom = SCR_RECT.bottom  # プレイヤーが画面の一番下
+        self.reload_timer = 0
     def update(self):
         # 押されているキーをチェック
         pressed_keys = pygame.key.get_pressed()
@@ -50,6 +55,29 @@ class Player(pygame.sprite.Sprite):
         elif pressed_keys[K_RIGHT]:
             self.rect.move_ip(self.speed, 0)
         self.rect.clamp_ip(SCR_RECT)
+        # ミサイルの発射
+        if pressed_keys[K_SPACE]:
+            # リロード時間が0になるまで再発射できない
+            if self.reload_timer > 0:
+                # リロード中
+                self.reload_timer -= 1
+            else:
+                # 発射！！！
+                Player.shot_sound.play()
+                Shot(self.rect.center)  # 作成すると同時にallに追加される
+                self.reload_timer = self.reload_time
+
+class Shot(pygame.sprite.Sprite):
+    """プレイヤーが発射するミサイル"""
+    speed = 9  # 移動速度
+    def __init__(self, pos):
+        # imageとcontainersはmain()でセット
+        pygame.sprite.Sprite.__init__(self, self.containers)
+        self.rect.center = pos  # 中心座標をposに
+    def update(self):
+        self.rect.move_ip(0, -self.speed)  # 上へ移動
+        if self.rect.top < 0:  # 上端に達したら除去
+            self.kill()
 
 if __name__ == "__main__":
     main()
